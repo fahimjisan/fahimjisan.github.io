4db9ff1ddcb2355d83b035ef0736d1da2f38d73e
fahimjisan <fahimjisan@yahoo.com>
2020-10-23 18:24:09 -0400

    change

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,7 @@
 // You may wish to find an effective randomizer function on MDN.
 
+const { urlencoded } = require("express");
+
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -17,6 +19,7 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -30,7 +33,28 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+      if (document.querySelector('.flex-inner')){
+        document.querySelector('.flex-inner').remove();
+      }
+      console.log(fromServer);
+      const newArr = range(10);
+      const newArr2 = newArr.map(() => {
+        const number = Math.floor()(Math.random() * 243);
+        return fromServer[number]
+      }
+      );
+
+      const reverseList = newArr2.sort((a,b) => sortFunction(b,a, 'name'));
+      const il = document.createElement('ul');
+      ul.className = 'flex-inner';
+      $('form').prepend(ul);
+
+      reverseList.forEach((el, i) =>{
+        const li = document.createElement('li');
+        $(li).append(`<input type= "checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        $(ul).append(li);
+      });
     })
     .catch((err) => console.log(err));
 });

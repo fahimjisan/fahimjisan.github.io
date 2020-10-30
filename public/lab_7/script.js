bae4c1bf74bf7b11f1afab5f88d239be31048c45
fahimjisan <fahimjisan@yahoo.com>
2020-10-30 16:40:34 -0400

    lab 7 change

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,65 +1,57 @@
-function convertRestaurantsToCategories(restauranList){
+function convertRestaurantsToCategories(restauranList) {
   const categoryArray = [];
   const result = {};
-  for(let i=0; i< restauranList.length; i += 1){
+  for (let i = 0; i < restauranList.length; i += 1) {
     categoryArray.push(restauranList[i].category);
-
   }
-  for (let i = 0; i < categoryArray.length; i+= 1){
-    if(!result[categoryArray[i]]) {
+  for (let i = 0; i < categoryArray.length; i += 1) {
+    if (!result[categoryArray[i]]) {
       result[categoryArray[i]] = 0;
-
     }
     result[categoryArray[i]] += 1;
   }
   const reply = Object.keys(result).map((category) => ({
     y: result[category],
-    label: category
+    label: category,
   }));
-  console.log('reply', reply);
+  console.log("reply", reply);
   return reply;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
- // CanvasJS.addColorSet("customColorSet1", [
+  // CanvasJS.addColorSet("customColorSet1", [
   // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
-  const chart = new CanvasJS.Chart('chartContainer', {
+  const chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
 
-
-    title:{
-      text: 'Fortune 500 Companies by Country'
+    title: {
+      text: "Fortune 500 Companies by Country",
     },
     axisX: {
-      interval:1
+      interval: 1,
     },
     axisY2: {
-      interLacedColor: 'rgba(1,77,101,.2',
-      gridColor: 'rgba(1,77,101,.1',
-      title: 'Number of Companies'
+      interLacedColor: "rgba(1,77,101,.2",
+      gridColor: "rgba(1,77,101,.1",
+      title: "Number of Companies",
     },
-    data: [{
-      type: 'bar',
-      name: 'companies',
-      axisYType: 'secoundary',
-      color: '#014D65',
-      dataPoints: datapointsFromRestaurantsList
-
-
-    }]
+    data: [
+      {
+        type: "bar",
+        name: "companies",
+        axisYType: "secoundary",
+        color: "#014D65",
+        dataPoints: datapointsFromRestaurantsList,
+      },
+    ]
   });
   chart.render();
-
 }
 
-
-
-
-
 function runThisWithResultsFromServer(jsonFromServer) {
-  console.log('jsonFromServer', jsonFromServer);
-  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
+  console.log("jsonFromServer", jsonFromServer);
+  sessionStorage.setItem("restaurantList", JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
@@ -71,7 +63,7 @@ function runThisWithResultsFromServer(jsonFromServer) {
 }
 
 // Leave lines 52-67 alone; do your work in the functions above
-document.body.addEventListener('submit', async (e) => {
+document.body.addEventListener("submit", async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
   fetch("/api", {
